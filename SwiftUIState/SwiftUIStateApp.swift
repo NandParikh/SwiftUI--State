4254723dca378f443cddf4c66894dc31037be389
NandParikh <nand.parikh.apple@gmail.com>
2025-11-24 12:32:51 +0530

    Initial Commit

---
+++ SwiftUIState/SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateApp.swift
+//  SwiftUIState
+//
+//  Created by                     Nand Parikh on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
