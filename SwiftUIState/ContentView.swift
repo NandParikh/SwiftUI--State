9c570fe7e845cb62dd314179d2c3e414c43c2bd6
NandParikh <nand.parikh.apple@gmail.com>
2025-11-24 18:20:45 +0530

    Initial commit

--- SwiftUIState/ContentView.swift
+++ SwiftUIState/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isPlaying = false
+    @State private var playStatus = "Not playing"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(spacing : 70){
+            Image(systemName: "music.note")
+                .resizable()
+                .frame(width: 300, height: 300)
+                .background(.gray.opacity(0.7))
+                .cornerRadius(20)
+            
+            Text(playStatus)
+                .font(.title)
+            
+            HStack(spacing : 70){
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "backward")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                }
+                
+                Button {
+                    isPlaying.toggle()
+                    playStatus = isPlaying ? "Playing" : "Not playing"
+                    
+                } label: {
+                    Image(systemName: isPlaying ? "pause" : "play")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                }
+                
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "forward")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                }
+            }
+            
+            
         }
-        .padding()
+        
     }
 }
 
